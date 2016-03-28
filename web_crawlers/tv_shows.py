b71c8b563c882e35d7d97c4e97df8a57ccf128e4
thedespicableknight <ranamihir@gmail.com>
2016-03-28 13:06:57 +0530

    Fixed typo

--- web_crawlers/tv_shows.py
+++ web_crawlers/tv_shows.py
@@ -9,7 +9,7 @@ tv_shows = {
     "Arrow": 'http://awesomedl.ru/tag/arrow/',
     "Better Caul Saul": 'http://awesomedl.ru/?s=better+call+saul&x=0&y=0',
     "Castle": 'http://awesomedl.ru/tag/castle/',
-    "Elementary": 'http://awesomedl.ru/tag/elemetary/',
+    "Elementary": 'http://awesomedl.ru/tag/elementary/',
     "House of Cards": 'http://awesomedl.ru/?s=house+of+cards&x=0&y=0',
     "Marvel's Agents of S.H.I.E.L.D": 'http://awesomedl.ru/?s=marvel%27s+agents+of+s.h.i.e.l.d.&x=0&y=0',
     "Marvel's Daredevil": 'http://awesomedl.ru/?s=marvel%27s+daredevil&x=0&y=0',
