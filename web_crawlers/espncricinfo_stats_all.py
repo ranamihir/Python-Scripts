3497d3ddf2b9cb5a67459342dd38eda297701df5
Mihir Rana <ranamihir@gmail.com>
2016-05-31 20:25:16 +0530

    Proper pagination for all stats + Files saved in Downloads folder

--- web_crawlers/espncricinfo_stats_all.py
+++ web_crawlers/espncricinfo_stats_all.py
@@ -1,6 +1,7 @@
 # This is a script that scrapes all the data from espncricinfo.com, based on user's input of class (Tests, ODI's, T20's,
 #  etc.) for all types (Batting, Bowling, Fielding, etc.), and dumps all the data into separate CSV files.
 import requests
+import os
 from bs4 import BeautifulSoup
 
 classes = {
@@ -15,30 +16,21 @@ classes = {
     'youth odis': 21
 }
 
-def maximum_pages(url):
-        source_code = requests.get(url)
-        plain_text = source_code.text
-        soup = BeautifulSoup(plain_text, "html.parser")
-        for row in soup.findAll('tr', {'class': 'data2'}):
-            for cell in row.findAll('td'):
-                text = cell.text
-                if 'Page' in text:
-                    return int(text.split()[-1])
-
 def spider(class_type):
     types = ['batting', 'bowling', 'fielding', 'allround', 'fow', 'team', 'official', 'aggregate']
     for type in types:
-        print('For file ' + class_type + '_' + type + '.csv:')
-        url = 'http://stats.espncricinfo.com/ci/engine/stats/index.html?template=results;type=' + type + ';class=' + str(classes[class_type])
-        max_pages = 1
-        max = maximum_pages(url)
-        if max:
-            max_pages = max
-        page = 1
-        f = open(class_type + '_' + type + '.csv', 'w+')
-        while page <= max_pages:
-            print('\r\tDownloading page ' + str(page) + ' of ' + str(max_pages) + '...', end='')
-            url += ';page=' + str(page)
+        if not os.path.exists('Downloads'):
+            os.makedirs('Downloads')
+        f = open('Downloads/' + class_type + '_' + type + '.csv', 'w+')
+        print('\nFor file ' + f.name + ':')
+        params = []
+        url = 'http://stats.espncricinfo.com/ci/engine/stats/index.html?template=results;type=' + type + ';class=' + str(classes[class_type]) + ";page=1"
+        while url:
+            params = url.split(';')
+            for item in params:
+                if 'page' in item:
+                    print('\r\t' + item.capitalize(), end='')
+                    break
             source_code = requests.get(url)
             plain_text = source_code.text
             soup = BeautifulSoup(plain_text, "html.parser")
@@ -50,7 +42,15 @@ def spider(class_type):
                         except:
                             pass
                 f.write('\n')
-            page+=1
+            pagination_links = soup.find_all('a', {'class': 'PaginationLink'})
+            if pagination_links:
+                for link in pagination_links:
+                    if link.text == 'Next \n':
+                        url = 'http://stats.espncricinfo.com/' + link['href']
+                        break
+                    else:
+                        url = None
+            else:
+                break
         f.close()
-
 spider(input('Enter class:\n').lower().replace('\'', ''))
