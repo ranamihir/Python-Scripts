2528fb746dcd480c0ef573219ca7a65245692a33
thedespicableknight <ranamihir@gmail.com>
2016-07-15 13:35:49 +0530

    Formalised download process.
    Formalised download process. Also added unarchiving feature, keeping
    only the video file and deleting the RAR file feature.
    Download percent is now printed.

--- web_crawlers/tv_shows.py
+++ web_crawlers/tv_shows.py
@@ -1,15 +1,18 @@
 # This is a script checks for new episodes of TV Shows, opens their download page in browser and starts downloading them automatically
-import requests
-import webbrowser
-import os
 from bs4 import BeautifulSoup
 from datetime import datetime
+from rarfile import RarFile
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import requests
 import time
+import os
 
+# List TV Shows
 tv_shows = {
     "Arrow": 'http://awesomedl.ru/tag/arrow/',
-    "Castle": 'http://awesomedl.ru/tag/castle/',
     "Elementary": 'http://awesomedl.ru/tag/elementary/',
     "House of Cards": 'http://awesomedl.ru/?s=house+of+cards&x=0&y=0',
     "Marvel's Agents of S.H.I.E.L.D": 'http://awesomedl.ru/?s=marvel%27s+agents+of+s.h.i.e.l.d.&x=0&y=0',
@@ -20,30 +23,51 @@ tv_shows = {
     "The Flash": 'http://awesomedl.ru/?s=the+flash&x=0&y=0'
 }
 
-target = open("C:/Users/ranamihir/Desktop/TV Shows.txt", 'w')
+# Declare global variable browser
+global browser
 
+# Check for new episodes, download them and extract the RAR file. Move the video file to E: and delete the RAR file.
 for show in tv_shows:
+    print('Checking for ' + show + '...')
     url = tv_shows[show]
-    source_code = requests.get(url)
-    plain_text = source_code.text
-    soup = BeautifulSoup(plain_text)
-    for dates in soup.findAll('span', {'class': 'meta_date'}):
-        if datetime.strptime(dates.text, '%B %d, %Y').date() == datetime.today().date():
-            target.write(show + " is here!\n")
-            episode_url = dates.find_previous("a")['href']
-            episode_source_code = requests.get(episode_url)
-            episode_plain_text = episode_source_code.text
-            episode_soup = BeautifulSoup(episode_plain_text)
-            download_url = episode_soup.find("a", text="Mega")['href']
-            browser = webdriver.Chrome("C:/Python34/chromedriver.exe")
-            browser.get(download_url[download_url.index("https")::])
-            time.sleep(15)
-            download_button = browser.find_element_by_class_name('throught-browser')
-            download_button.click()
+    try:
+        source_code = requests.get(url)
+        plain_text = source_code.text
+        soup = BeautifulSoup(plain_text, "html5lib")
+        for dates in soup.findAll('span', {'class': 'meta_date'}):
+            if datetime.strptime(dates.text, '%B %d, %Y').date() == datetime.today().date():
+                print('\n' + show + ' is here!')
+                episode_url = dates.find_previous('a')['href']
+                episode_source_code = requests.get(episode_url)
+                episode_plain_text = episode_source_code.text
+                episode_soup = BeautifulSoup(episode_plain_text, "html5lib")
+                download_url = episode_soup.find('a', text='Mega')['href']
+                browser = webdriver.Chrome('C:/Python34/chromedriver.exe')
+                browser.get(download_url[download_url.index('https')::])
+                download_button = WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.CLASS_NAME, 'throught-browser')))
+                download_button.click()
+                filename = browser.find_element_by_class_name('filename').get_attribute('title')
+                print('Downloading ' + filename + '...')
+                while 1:
+                    download_percent = browser.find_element_by_class_name('percents-txt').text
+                    print('\r' + download_percent + ' completed.', end='')
+                    if download_percent == '100 %':
+                        time.sleep(30)
+                        with RarFile('C:/Users/ranamihir/Downloads/' + filename) as rf:
+                            for f in rf.infolist():
+                                if f.filename[::-1].split('.')[0] != 'txt':
+                                    with open('E:/' + f.filename, 'wb') as of:
+                                        of.write(rf.read(f))
+                                    break
+                        os.remove('C:/Users/ranamihir/Downloads/' + filename)
+                        break
+                print()
+    except Exception as e:
+        print('\nError :' + str(e))
+        pass
 
-target.close()
-
-if os.stat("C:/Users/ranamihir/Desktop/TV Shows.txt").st_size:
-    webbrowser.open("TV Shows.txt")
-else:
-    os.remove("C:/Users/ranamihir/Desktop/TV Shows.txt")
+# Safely quit browser
+try:
+    browser.quit()
+except:
+    pass
--- web_crawlers/tv_shows_linux.py
+++ web_crawlers/tv_shows_linux.py
@@ -1,14 +1,18 @@
-# This is a script checks for new TV Show episodes, and starts downloading them automatically
-import requests
-import os
+# This is a script checks for new episodes of TV Shows, opens their download page in browser and starts downloading them automatically
 from bs4 import BeautifulSoup
 from datetime import datetime
+from rarfile import RarFile
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import requests
 import time
+import os
 
+# List TV Shows
 tv_shows = {
     "Arrow": 'http://awesomedl.ru/tag/arrow/',
-    "Castle": 'http://awesomedl.ru/tag/castle/',
     "Elementary": 'http://awesomedl.ru/tag/elementary/',
     "House of Cards": 'http://awesomedl.ru/?s=house+of+cards&x=0&y=0',
     "Marvel's Agents of S.H.I.E.L.D": 'http://awesomedl.ru/?s=marvel%27s+agents+of+s.h.i.e.l.d.&x=0&y=0',
@@ -19,21 +23,52 @@ tv_shows = {
     "The Flash": 'http://awesomedl.ru/?s=the+flash&x=0&y=0'
 }
 
+# Declare global variable browser
+global browser
+
+# Check for new episodes, download them and extract the RAR file. Move the video file to E: and delete the RAR file.
 for show in tv_shows:
+    print('Checking for ' + show + '...')
     url = tv_shows[show]
-    source_code = requests.get(url)
-    plain_text = source_code.text
-    soup = BeautifulSoup(plain_text, "lxml")
-    for dates in soup.findAll('span', {'class': 'meta_date'}):
-        if datetime.strptime(dates.text, '%B %d, %Y').date() == datetime.today().date():
-            os.system('notify-send \"' + show + ' is here!\" --urgency=critical')
-            episode_url = dates.find_previous("a")['href']
-            episode_source_code = requests.get(episode_url)
-            episode_plain_text = episode_source_code.text
-            episode_soup = BeautifulSoup(episode_plain_text, "lxml")
-            download_url = episode_soup.find("a", text="Mega")['href']
-            browser = webdriver.Chrome()
-            browser.get(download_url[download_url.index("https")::])
-            time.sleep(15)
-            download_button = browser.find_element_by_class_name('throught-browser')
-            download_button.click()
+    try:
+        source_code = requests.get(url)
+        plain_text = source_code.text
+        soup = BeautifulSoup(plain_text, "html.parser")
+        for dates in soup.findAll('span', {'class': 'meta_date'}):
+            if datetime.strptime(dates.text, '%B %d, %Y').date() == datetime.today().date():
+                os.system('notify-send \"' + show + ' is here!\" --urgency=critical')
+                print('\n' + show + ' is here!')
+                episode_url = dates.find_previous('a')['href']
+                episode_source_code = requests.get(episode_url)
+                episode_plain_text = episode_source_code.text
+                episode_soup = BeautifulSoup(episode_plain_text, "html.parser")
+                download_url = episode_soup.find('a', text='Mega')['href']
+                browser = webdriver.Chrome()
+                browser.get(download_url[download_url.index('https')::])
+                download_button = WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.CLASS_NAME, 'throught-browser')))
+                download_button.click()
+                filename = browser.find_element_by_class_name('filename').get_attribute('title')
+                print('Downloading ' + filename + '...')
+                while 1:
+                    download_percent = browser.find_element_by_class_name('percents-txt').text
+                    print('\r' + download_percent + ' completed.', end='')
+                    if download_percent == '100 %':
+                        time.sleep(30)
+                        with RarFile('/home/mihir/Downloads/' + filename) as rf:
+                            for f in rf.infolist():
+                                if f.filename[::-1].split('.')[0] != 'txt':
+                                    with open('/media/mihir/Entertainment/' + f.filename, 'wb') as of:
+                                        of.write(rf.read(f))
+                                    break
+                        os.remove('/home/mihir/Downloads/' + filename)
+                        break
+                print()
+    except Exception as e:
+        print('\nError :' + str(e))
+        pass
+
+# Safely quit browser
+try:
+    browser.quit()
+except:
+    pass
